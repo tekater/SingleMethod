e1df6ae7ae797ecfc614d29cdc37a9675a449a98
tekater <tekateplay1@yandex.ru>
2023-02-17 13:58:47 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        SingleMethodInterface instance = new SingleMethodInterface() {
+            @Override
+            public void doSomething() {
+                System.out.println("Анонимный класс производит какую-то работу");
+            }
+        };
+                instance.doSomething();
+    }
+}
---
+++ src/SingleMethodInterface.java
@@ -0,0 +1,4 @@
+public interface SingleMethodInterface {
+
+    void doSomething();
+}
